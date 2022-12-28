a2a27f37e3517e1f30c73f050d8f7dfa361fcb12
Karan Patel <84426799+Karanpatel-15@users.noreply.github.com>
2022-12-28 00:16:44 -0500

    Add files via upload

---
+++ client/build/precache-manifest.3f28d8ce056888c06344da50f32e58b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3877773b892aa477eb3a7db34b7955e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad852fddd7d988349b2f",
+    "url": "/static/css/main.d3f13e23.chunk.css"
+  },
+  {
+    "revision": "0ee4f30bdde23470fefb",
+    "url": "/static/js/2.011c3ce8.chunk.js"
+  },
+  {
+    "revision": "038db422c0414d8dfd31541a1bf2a15e",
+    "url": "/static/js/2.011c3ce8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad852fddd7d988349b2f",
+    "url": "/static/js/main.93b0250a.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.93b0250a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
